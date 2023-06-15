212e4e2cec6be94b63f79ae7defa8622161a69cd
dell <dell@Shukranti>
2023-06-15 14:58:24 +0530

    1commit

---
+++ shukranti/src/shukranti/practice1.java
@@ -0,0 +1,11 @@
+package shukranti;
+
+public class practice1 {
+
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
---
+++ shukranti/src/shukranti/project.java
@@ -0,0 +1,8 @@
+package shukranti;
+
+public class project {
+public static void main(String[] args) {
+	System.out.println("hello");
+}
+	
+}
